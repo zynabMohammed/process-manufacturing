ab27e9e5a5353bd76b765ebb9af6bcf2fffb6681
zynabMohammed <77670757+zynabMohammed@users.noreply.github.com>
2021-06-02 19:55:34 +0200

    change doctype name

--- process_manufacturing/process_manufacturing/doctype/process_definition/bill_of_material2.js
+++ process_manufacturing/process_manufacturing/doctype/process_definition/bill_of_material2.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2018, earthians and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Process Definition', {
+frappe.ui.form.on('Bill Of Material2', {
 	refresh: function(frm) {
 
 	},
